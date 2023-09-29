2882870e42232600db77984fdbe70a089779ccdd
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-09-29 12:14:46 +0530

    Update 01_variableAssign.js

--- 01_variableAssign.js
+++ 01_variableAssign.js
@@ -4,3 +4,25 @@ console.log(myFullName);
 
 var myLove= "I Love only JS";
 console.log(myLove);
+
+var firstName = "firstName";
+console.log(firstName);
+
+var lastName = "lastName";
+console.log(lastName);
+
+var age="age";
+console.log(age);
+
+var address="address";
+console.log(address);
+
+var pincode="pinCode";
+console.log(pincode);
+
+var gender="gender";
+console.log(gender);
+
+var ismarried="isMarried";
+console.log(ismarried);
+
