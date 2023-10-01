cbe61531f7bad6f61a766216511b13ce2fd037b8
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-01 17:57:59 +0530

    Create 02_typeOf.js

---
+++ 02_typeOf.js
@@ -0,0 +1,32 @@
+console.log("==========Assignment no 2=========");
+console.log("\\\\\\\\\\\\\\\\\Step 1///////////");
+console.log("---------Before Initialization---------");
+var bankName;
+console.log("bankName=",bankName, "; Data Type=", typeof bankName);
+
+console.log("---------After Initialization---------");
+var bankName ="SBI Bank";
+console.log("bankName=",bankName, "; Data Type=", typeof bankName);
+
+
+console.log("\\\\\\\\\\\\\\\\\Step 2///////////");
+var marks="90%";
+console.log("marks=", marks, "; Data Type= ", typeof marks);
+
+console.log("\\\\\\\\\\\\\\\\\Step 3///////////");
+console.log("---------Before Initialization---------");
+var isWorking;
+console.log("isWorking=",isWorking, "; Data Type=",typeof isWorking);
+
+console.log("---------After Initialization---------");
+var isWorking= true;
+console.log("isWorking=",isWorking, "; Data Type=",typeof isWorking);
+
+console.log("\\\\\\\\\\\\\\\\\Step 4///////////");
+console.log("*******A******");
+var totalCount="Hundred and Seven";
+console.log("Total Count=", totalCount, "; Data Type=", typeof totalCount );
+
+
+var totalCount= 107;
+console.log("Total Count=", totalCount, "; Data Type=", typeof totalCount );
