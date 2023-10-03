e677055f27d282efcee74217f6a0988c65f1b74b
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-03 11:54:32 +0530

    Create 04_stringBasics.js

---
+++ 04_stringBasics.js
@@ -0,0 +1,29 @@
+
+var greet = 'Good Morning';
+console.log("===============Length property================");
+var totalLenght =greet.length;
+console.log('Lengthof the string is: ', totalLenght);
+
+
+console.log("========== charAt()==============");
+var charAt0 = greet.charAt(0);
+console.log("Character at index 0is:",charAt0);
+
+console.log("========== charAt()==============");
+var charAt5 = greet.charAt(5);
+console.log("Character at index 0is:",charAt5);
+
+var lastIndexChar = greet.charAt(greet.totalLenght-3);
+console.log("Lastindex char is  ", lastIndexChar);
+
+console.log("========== concatination==============");
+var firstName = 'Nikhil';
+var lastName = '  Sapkal';
+var result = firstName.concat(lastName);
+console.log(result);
+
+console.log("========== indexOf==============");
+var greet.indexOf("M");
+console.log("Index of M is  ", indexOfM);
+
+
