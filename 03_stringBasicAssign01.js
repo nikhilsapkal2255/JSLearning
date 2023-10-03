daba5b484ede968a4ab19978fbe144255dae7fe9
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-03 16:56:24 +0530

    Update 03_stringBasicAssign01.js

--- 03_stringBasicAssign01.js
+++ 03_stringBasicAssign01.js
@@ -5,6 +5,7 @@ console.log("**************Step 2******************");
 var hobby1 = " Programming.";
 console.log("hobby1 : ", hobby1);
 var hobby2 = " Riding-Bike.";
+
 console.log("hobby2 : ", hobby2);
 var hobby3 = " Travelling.";
 console.log("hobby3 : ", hobby3);
