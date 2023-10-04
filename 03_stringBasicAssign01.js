ccd9f31bfe5893ff48f81d738f7a7f23e084d8ae
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-04 18:26:34 +0530

    updated

--- 03_stringBasicAssign01.js
+++ 03_stringBasicAssign01.js
@@ -1,13 +1,11 @@
-console.log("===============Assignment no 3================");
+console.log("===============Assignment no 1 nikhilsapkal================");
 console.log("***************Step 1*****************");
 console.log("My dream Company is Tata Consultancy Services");
 console.log("**************Step 2******************");
 var hobby1 = " Programming.";
-console.log("hobby1 : ", hobby1);
 var hobby2 = " Riding-Bike.";
-
-console.log("hobby2 : ", hobby2);
 var hobby3 = " Travelling.";
-console.log("hobby3 : ", hobby3);
+console.log(`hobby1 : , ${hobby1}, hobby2 : , ${hobby2} hobby3 : , ${hobby3}`);
+console.log("**************Step 3******************");
 var addition = hobby1.concat(hobby2).concat(hobby3);
-console.log(addition);                
+console.log(`Result of String concatenantion is :  ${addition}`);                
