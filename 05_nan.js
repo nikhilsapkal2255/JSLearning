b49fa5b0b0854a053d2a7396c69ef729bfc97a8a
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-06 00:11:52 +0530

    Update 05_nan.js

--- 05_nan.js
+++ 05_nan.js
@@ -2,3 +2,14 @@ var n1=0;
 var n2=0;
 var result =n1/n2;
 console.log(result);
+console.log(typeof result);
+
+var num = "20";
+console.log(typeof num);
+var result = +num;
+console.log(result); 
+
+var num1 = "Jenny";
+console.log(typeof num1);
+var result = +num1;
+console.log(result); 
