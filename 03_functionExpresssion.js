fa54ddb25fa45b288ebe235e2f02cf0c4620b1fc
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-06 07:38:25 +0530

    Update 03_functionExpresssion.js

---
+++ 03_functionExpresssion.js
@@ -0,0 +1,18 @@
+
+
+var show = function(){
+    console.log(`Inside function Expressiom`);
+}
+show();
+
+var add = function(value1, value2){
+    console.log(value1+value2);
+}
+add(20,10);
+
+var square = function(value){
+    return result = value* value;
+    console.log(result);
+}
+var result = square(25);
+console.log("Square of 25 is: ",result);
