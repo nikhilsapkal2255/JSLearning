c93cd5913f703d725dd3f45619fff9dd149d577d
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-06 11:41:24 +0530

    u

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -1,17 +1,29 @@
 function squareOfWordLength(str) { 
  var lengthOfStr =  str.length;
  var result = lengthOfStr*lengthOfStr;
- console.log(result);
+ //console.log(result);
  return result;
 }
+console.log(`****************STEP 1***************`);
+
 var result = squareOfWordLength("Javascript");
+console.log(`1.1. Given string is "JavaScript" and its square of length is:${result}`);
+
 var result = squareOfWordLength("Google Chrome");
+console.log(`1.2. Given string is "Google Chrome" and its square of length is:${result}`);
+
 var result = squareOfWordLength("Developer Smart");
+console.log(`1.3. Given string is "JavaScript" and its square of length is:${result}`);
+
+console.log(`****************STEP 2***************`);
 
 function givenStr(){
     var givenStr = "I am Angular Developer";
-    var result1 = givenStr.length / 4;     
-    console.log(`2.1.Result is ${result1}`);
+    var words = givenStr.split(" ");
+    var averageLength = givenStr.length / words.length;
+    var multiply = givenStr.length * words.length;     
+    console.log(`2.1. the string length and divide by total number words a Result is ${averageLength}`);
+    console.log(`2.2. the string length and multiple by the total words ${multiply}`);
 }
 
 var result1 = givenStr("I am Angular Developer")
