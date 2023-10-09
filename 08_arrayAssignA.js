4aa1a66d47522e189c2184cdf6df51a1a78b851a
nikhilsapkal2255 <nikhilsapkal2255@gmail.com>
2023-10-10 00:01:19 +0530

    upd

---
+++ 08_arrayAssignA.js
@@ -0,0 +1,4 @@
+var arrayFruits = ["Banana","Orange","Apple",'Mango','Watermelon'];
+// let arrayFruits = [Banana, Orange ,Apple ,Mango ,Watermelon];
+console.log(arrayFruits[0]);
+console.log(arrayFruits-1);
---
+++ 09_object.js
@@ -0,0 +1,29 @@
+
+
+
+// object creation using object literals
+let person = {
+    name: "Jenny",
+    age: "22",
+    city: "Pune",
+    isMarried: true,
+}
+console.log(typeof person);
+console.log(person);
+
+//property access
+// 1. Dot Notation
+let personName = person.name;
+console.log(`Person name is ${personName}`);
+
+console.log(`Person age is : ${person.age}`);
+
+//1. [] Notation
+let nameOfPerson = person["name"]
+console.log(`Person name is: ${nameOfPerson}`);
+
+console.log(`Adding property in object`);
+person.city="Pune";
+console.log(person);
+console.table(person);
+
